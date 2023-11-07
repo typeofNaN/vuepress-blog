ce08d274dea518c7305da5c2e6e1736cca53dab5
typeofNaN <sixnoodle@163.com>
2023-11-07 03:20:32 +0000

    Deploying to gh-pages from @ typeofNaN/vuepress-blog@e44d5bf9d2340fd3bdd9e9f20050cc714e4b7729 🚀

---
+++ assets/pageview-4865c4b2.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-4b7f89be.js";const o=async()=>{try{const{pageviewCount:e}=await r(()=>import("./pageview_vuepress-plugin-comment2-b25bb000.js"),[]);return e({serverURL:COMMENT_OPTIONS.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
